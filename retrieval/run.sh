e62ab0965b46141e9e9dbfc21a460ddf643db464
ifff <fanff.pku@gmail.com>
2015-04-19 16:40:46 +0800

    modify web

--- retrieval/run.sh
+++ retrieval/run.sh
@@ -11,20 +11,22 @@ expand=../query/issue/
 #expand=/index12/query/freebase/
 #expand=/index12/query/origin/
 #expand=/index12/query/issue/
-alpha=0.5
+alpha=0.4
 beta=0.5
 doc=5
 term=7
-for alpha in 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
+for doc in 3 4 5 6 7 8 9; do
+for term in 3 4 5 6 7 8 9 10; do
+#for alpha in 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
 ##for beta in 0.5; do
     output=../result/dynamic/RTRM/issue_alpha${alpha}_beta${beta}_doc${doc}_term${term}/
     #output=result2012/QEFB
     #output=result2012/knowledgeTerm 
     mkdir $output
-    total=10
+    total=5
     for((remain=0; remain<total; remain++)); do 
         ./main -index $index -ssf $ssf -query $query -expand $expand -output $output -count 60 -year $year -term $term -doc $doc -alpha $alpha -beta $beta -remain $remain -total $total & 
     #    ./main -index $index -ssf $ssf -query $query -expand $expand -output $output -count 60 -year $year -term $term -doc $doc -alpha $alpha -beta $beta -remain $remain -total $total&
     done;
 done;
- 
+done; 
